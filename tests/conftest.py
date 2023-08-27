ff24e427d09bfa8cf873c818473db534af5e46ad
albina-pasichnyk <albina.pasichnyk@gmail.com>
2023-08-27 16:51:03 +0200

    update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from contextlib import suppress
 # import allure
 import pytest
 
-from api_collections.auth_api import AuthAPI
+# from api_collections.auth_api import AuthAPI
 from api_collections.booking_api import BookingAPI
 from api_collections.data_classes.booking_data import Booking
 from constants import ROOT_DIR
@@ -14,12 +14,12 @@ from utilities.config_object import ConfigObject
 from utilities.driver_factory import create_driver_factory
 
 
-@pytest.hookimpl(hookwrapper=True, tryfirst=True)
-def pytest_runtest_makereport(item, call):
-    outcome = yield
-    rep = outcome.get_result()
-    setattr(item, "rep_" + rep.when, rep)
-    return rep
+# @pytest.hookimpl(hookwrapper=True, tryfirst=True)
+# def pytest_runtest_makereport(item, call):
+#     outcome = yield
+#     rep = outcome.get_result()
+#     setattr(item, "rep_" + rep.when, rep)
+#     return rep
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -37,11 +37,11 @@ def create_driver(environment, request):
     driver.maximize_window()
     driver.get(environment.base_url)
     yield driver
-    if request.node.rep_call.failed:
-        with suppress(Exception):
-            allure.attach(driver.get_screenshot_as_png(),
-                          name=request.function.__name__,
-                          attachment_type=allure.attachment_type.PNG)
+    # if request.node.rep_call.failed:
+    #     with suppress(Exception):
+    #         allure.attach(driver.get_screenshot_as_png(),
+    #                       name=request.function.__name__,
+    #                       attachment_type=allure.attachment_type.PNG)
     driver.quit()
 
 
