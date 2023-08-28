0bfae554c7a996134c4f6841b2f2002cf7b936f0
albina-pasichnyk <albina.pasichnyk@gmail.com>
2023-08-28 20:19:54 +0200

    Add options for CI run

--- utilities/driver_factory.py
+++ utilities/driver_factory.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 CHROME = 1
 SAFARI = 2
 FIREFOX = 3
 
+options = Options()
+options.headless = True
+options.add_argument('--no-sandbox')
+
 
 def create_driver_factory(driver_id):
     if int(driver_id) == CHROME:
         driver = webdriver.Chrome()
+        # driver = webdriver.Chrome(options=options)
         return driver
     elif int(driver_id) == SAFARI:
         driver = webdriver.Safari()
